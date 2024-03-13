89192b76c5d0163225ef673f6adbd7f070a3cd2b
omognuni <melbong0069@gmail.com>
2024-03-13 21:21:41 +0900

    [Feat] request

--- app/clinic/selectors.py
+++ app/clinic/selectors.py
@@ -1,4 +1,4 @@
-from clinic.models import Doctor
+from clinic.models import Doctor, TreatmentRequest
 from django.db.models import Q
 
 
@@ -15,17 +15,24 @@ def filter_doctors(filters):
                 | Q(departments__name__icontains=keyword)
                 | Q(treatments__name__icontains=keyword)
             )
-            doctor_queryset = doctor_queryset.filter(conditions)
+            doctor_queryset = doctor_queryset.filter(conditions).distinct()
 
-        return doctor_queryset.distinct()
     if time:
         day = time.weekday()
         time = time.time()
         doctor_queryset = doctor_queryset.filter(
             hours__day=day, hours__opening_time__lte=time, hours__closing_time__gte=time
         )
-        return doctor_queryset
+
     return doctor_queryset
+
+
+def filter_requests(filters):
+    doctor_id = filters.get("doctor_id", None)
+    request_queryset = TreatmentRequest.objects.all()
+    if doctor_id:
+        request_queryset = request_queryset.filter(doctor_id=doctor_id)
+    return request_queryset
 
 
 def get_doctors(filters=None):
@@ -34,3 +41,11 @@ def get_doctors(filters=None):
     doctor_queryset = filter_doctors(filters)
 
     return doctor_queryset
+
+
+def get_requests(filters=None):
+    filters = filters or {}
+
+    request_queryset = filter_requests(filters)
+
+    return request_queryset
